c1f38e85b2bcdd80a81289b24ccf5acb224b2086
Michael Berman <me@scripti.st>
2015-11-08 08:53:03 +0100

    Moved scss to separate directory

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
   sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  gulp.src('./public/css/*.scss')
+  gulp.src('./public/scss/*.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(gulp.dest('./public/css'))
@@ -13,7 +13,7 @@ gulp.task('sass', function () {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./public/css/*.scss', ['sass']);
+  gulp.watch('./public/scss/*.scss', ['sass']);
 });
 
 gulp.task('develop', function () {
