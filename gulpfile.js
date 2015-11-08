437977c61c035c0154cb2ae308dd33296b91a79e
Michael Berman <me@scripti.st>
2015-11-08 09:13:26 +0100

    Improved indentation of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,21 @@ var gulp = require('gulp'),
 
 gulp.task('coffee', function () {
 	gulp.src('./public/coffee/sneaquence.coffee', { read: false })
-	.pipe(plumber())
-	.pipe(browserify({ transform: ['coffeeify'], extensions: ['.coffee'] }))
-	.pipe(rename(function(path) {
-		path.extname = '.js'
-	}))
-	.pipe(gulp.dest('./public/js'))
-	.pipe(livereload());
+		.pipe(plumber())
+		.pipe(browserify({ transform: ['coffeeify'], extensions: ['.coffee'] }))
+		.pipe(rename(function(path) {
+			path.extname = '.js'
+		}))
+		.pipe(gulp.dest('./public/js'))
+		.pipe(livereload());
 });
 
 gulp.task('sass', function () {
 	gulp.src('./public/scss/**/*.scss')
-	.pipe(plumber())
-	.pipe(sass())
-	.pipe(gulp.dest('./public/css'))
-	.pipe(livereload());
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(gulp.dest('./public/css'))
+		.pipe(livereload());
 });
 
 gulp.task('watch', function() {
